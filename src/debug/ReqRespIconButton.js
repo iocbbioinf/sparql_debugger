af57df5d63e4306d0f07e3cb66a0821b00d2c56b
marek <moos@uochb.cas.cz>
2024-07-17 15:56:35 +0200

    Preview length: chars -> bytes

--- src/debug/ReqRespIconButton.js
+++ src/debug/ReqRespIconButton.js
@@ -16,7 +16,7 @@ function ReqRespIconButton({ queryId, nodeId, isRequest }) {
   const [fileContent, setFileContent] = useState('');
   const [fileBlob, setFileBlob] = useState(null);
 
-  const PREVIEW_LENGTH = 2000; // Number of characters to show in preview
+  const PREVIEW_LENGTH = 4000; // Number of bytes to show in preview
 
   const fetchPreviewContent = useCallback(async (queryId, callId, isRequest) => {
     const reqResp = isRequest ? "request" : "response";
